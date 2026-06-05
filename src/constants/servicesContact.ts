63b612f41710c346d457c53f7ada67313385c90c
ghillantdimisqi <ghillantdimisqi@gmail.com>
2026-06-05 19:34:49 +0700

    tambahan

---
+++ src/constants/servicesContact.ts
@@ -0,0 +1,9 @@
+const servicesContact = [
+  "Web Development",
+  "Cloud Solutions",
+  "Mobile App Development",
+  "Software Development",
+  "UI/UX Design",
+  "Other",
+];
+export default servicesContact;
---
+++ src/constants/socials.ts
@@ -0,0 +1,26 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTiktok,
+  
+} from "react-icons/fa";
+const socials = [
+  {
+    href: "#",
+    icon: FaFacebookF,
+  },
+  {
+    href: "#",
+    icon: FaInstagram,
+  },
+  {
+    href: "#",
+    icon: FaLinkedinIn,
+  },
+  {
+    href: "#",
+    icon: FaTiktok,
+  },
+];
+export default socials;
